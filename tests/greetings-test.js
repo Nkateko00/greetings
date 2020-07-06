d0b9d1cfb1434f1e3356ef8c941b4e99a798296f
Nkateko00 <tekonku@icloud.com>
2020-07-06 17:28:48 +0200

    tests

--- tests/greetings-test.js
+++ tests/greetings-test.js
@@ -1,22 +1,22 @@
 describe("The greetUser Function",function(){
 it("It should greet Cameron correctly",function(){
     var greetPerson = greetMeFactory()
-    assert.equal("Hello,Cameron",greetMeFactory.greetUser("Cameron","English"));
+    assert.equal("Hello, Cameron",greetMeFactory().greetUser("Cameron","English"));
 
 });
 it("It should greet Aubrey in Xitsonga correctly",function(){
     var greetPerson = greetMeFactory()
-    assert.equal("Ahe,Aubrey",greetMeFactory.greetUser("Aubrey","Xitsonga"));
+    assert.equal("Ahe, Aubrey",greetMeFactory().greetUser("Aubrey","Xitsonga"));
 
 });
 it("It should greet Valerie in French correctly",function(){
     var greetPerson = greetMeFactory()
-    assert.equal("Bonjour,Valerie",greetMeFactory.greetUser("Valerie","French"));
+    assert.equal("Bonjour, Valerie",greetMeFactory().greetUser("Valerie","French"));
 
 });
 it("It should greet in default correctly",function(){
     var greetPerson = greetMeFactory()
-    assert.equal("Hi,Valerie",greetMeFactory.greetUser("Valerie","English"));
+    assert.equal("Hi, Valerie",greetMeFactory().greetUser("Valerie","default"));
 
 });
 }); 
