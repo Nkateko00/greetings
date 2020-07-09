9e0ecbee3d86ee4dce5ddd344983a668ab1dfcd2
Nkateko00 <tekonku@icloud.com>
2020-07-09 15:59:22 +0200

    additions

--- greet.js
+++ greet.js
@@ -1,13 +1,16 @@
-window.addEventListener("DOMContentLoaded",function(){
-    const greetButton = document.getElementById("greetButton");
-    const input = document.getElementById("inputElement");
-    const message = document.getElementById("alert");
-    const myCounter = document.getElementById("counter");
-    const resetButton = document.getElementById("resetButton");
+const greetButton = document.getElementById("greetButton");
+const input = document.getElementById("inputElement");
+const message = document.getElementById("alert");
+const myCounter = document.getElementById("counter");
+const resetButton = document.getElementById("resetButton");
     
-    var data = localStorage['greetedUsers'] ?JSON.parse(localStorage[greetedUsers]) : {};
+var data = localStorage['greetedUsers'] ?JSON.parse(localStorage[greetedUsers]) : {};
     
-    var greetings = greetMeFactory(data);
+var greetings = greetMeFactory(data);
 
-    
-});
+function greetRequest(){
+
+
+
+}
+greetButton.addEventListener("click",greetRequest())
